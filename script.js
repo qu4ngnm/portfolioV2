fc84e9b712f1b6ead3e86bc317fcdbeadf8e10ab
quangdaik2362001 <minhquangnguyen7@gmail.com>
2021-10-30 11:40:41 +0700

    Portfolio Ver2

---
+++ script.js
@@ -0,0 +1,66 @@
+var textWrapper = document.querySelector(".title");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".title .letter",
+    scale: [4, 1],
+    opacity: [0, 1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70 * i,
+  })
+  .add({
+    targets: ".title",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
+
+
+
+
+let elements = document.querySelectorAll('.rolling-text');
+
+elements.forEach(element => {
+  let innerText = element.innerText;
+  element.innerHTML = '';
+  
+  let textContainer = document.createElement('div');
+  textContainer.classList.add('block');
+  
+  for (let letter of innerText) {
+    let span = document.createElement('span');
+    span.innerText = letter.trim() === '' ? '\xa0': letter;
+    span.classList.add('letter');
+    textContainer.appendChild(span);
+  }
+  
+  element.appendChild(textContainer);
+  element.appendChild(textContainer.cloneNode(true));
+});
+
+
+
+
+
+// for presentation purpose
+setTimeout(() => {
+  elements.forEach(element => {
+    element.classList.add('play');
+  })
+}, 600);
+
+elements.forEach(element => {
+  element.addEventListener('mouseover', () => {
+    element.classList.remove('play');
+  });
+});
+
+
